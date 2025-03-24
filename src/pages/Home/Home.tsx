b17c5a422ebe168336d2d91517165f51d0e8fb97
ne1up <ohearn.thomas@gmail.com>
2025-03-23 22:00:02 -0700

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -91,7 +91,7 @@ export function Home({ userId }: HomeProps) {
           mx: 'auto',
           maxWidth: theme.breakpoints.values.sm,
           textAlign: 'center',
-          fontSize: 'clamp(50px, 8vw, 115px)',
+          fontSize: 'clamp(50px, 8vw, 100px)',
           fontWeight: 'bold', // Optional for styling
           lineHeight: 1,
           width: '100%',
